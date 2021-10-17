040ae60924fb4ed2c28329ca6684b9bc63d60d2b
hoangdesu <hoangdesu@gmail.com>
2021-10-17 14:42:52 +0700

    props

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,12 +1,64 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+    // const birthday = new Date(2021, 1, 27); // = 2021-02-26 :D?
+    // const expenseTitle = 'Curnon watch';
+    // const price = 3000000;
+    // const formattedPrice = price.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
+
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Curnon watch',
+            price: 3000000,
+            date: new Date(2021, 1, 27),
+        },
+        {
+            id: 'e2',
+            title: 'Keychron K2',
+            price: 5500000,
+            date: new Date(2020, 11, 15),
+        },
+        {
+            id: 'e3',
+            title: 'Delux mouse',
+            price: 250000,
+            date: new Date(2021, 6, 10),
+        },
+        {
+            id: 'e1',
+            title: 'Airpods 2',
+            price: 5000000,
+            date: new Date(2021, 5, 30),
+        },
+    ];
 
     return (
         <div>
             <h2>Let's get started!</h2>
-            <p>Finally learning React yayy</p>
-            <ExpenseItem /> {/* preferred */}
+            {/* <p>Finally learning React yayy</p> */}
+            
+            <ExpenseItem 
+                title={expenses[0]['title']}
+                price={expenses[0].price}
+                date={expenses[0].date}
+            /> {/* self-closing is preferred */}
+            <ExpenseItem
+                title={expenses[1]['title']}
+                price={expenses[1].price}
+                date={expenses[1].date}
+            />
+            <ExpenseItem
+                title={expenses[2]['title']}
+                price={expenses[2].price}
+                date={expenses[2].date}
+            />
+            <ExpenseItem
+                title={expenses[3]['title']}
+                price={expenses[3].price}
+                date={expenses[3].date}
+            />
+            
             {/* <ExpenseItem></ExpenseItem> */}
         </div>
     );
--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const birthday = new Date(2021, 1, 27); // = 2021-02-26 :D? 
-    const expenseTitle = 'Curnon watch';
-    const price = 3000000;
-    const formattedPrice = price.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
+function ExpenseItem(props) {
+    // const birthday = new Date(2021, 1, 27); // = 2021-02-26 :D?
+    // const expenseTitle = 'Curnon watch';
+    // const price = 3000000;
+    // const formattedPrice = price.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
 
     return (
         <div className="expense-item">
-            <div>{birthday.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{formattedPrice}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.price}</div>
             </div>
         </div>
     );
