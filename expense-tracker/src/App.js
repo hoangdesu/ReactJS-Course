07c051f410df32d1ae69a460dd9d1da405bc3710
hoangdesu <hoangdesu@gmail.com>
2021-10-18 02:25:33 +0700

    Expenses component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,11 +1,37 @@
 import Expenses from './components/Expenses';
 
 function App() {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Curnon watch',
+            price: 3000000,
+            date: new Date(2021, 1, 26),
+        },
+        {
+            id: 'e2',
+            title: 'Keychron K2',
+            price: 5500000,
+            date: new Date(2020, 11, 15),
+        },
+        {
+            id: 'e3',
+            title: 'Delux mouse',
+            price: 250000,
+            date: new Date(2021, 6, 10),
+        },
+        {
+            id: 'e1',
+            title: 'Airpods 2',
+            price: 5000000,
+            date: new Date(2021, 5, 30),
+        },
+    ];
 
     return (
         <div>
-            <h2>Let's get started!</h2>
-            <Expenses />
+            <h2>Expense Tracker</h2>
+            <Expenses expenses={expenses} />
         </div>
     );
 }
