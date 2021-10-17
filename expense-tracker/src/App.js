fa03eb926045fcb7d87448ac63e759d83d0fa608
hoangdesu <hoangdesu@gmail.com>
2021-10-17 14:22:14 +0700

    JSX expressions

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,14 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>Finally learning React yayy</p>
-      <ExpenseItem /> {/* preferred */}
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <p>Finally learning React yayy</p>
+            <ExpenseItem /> {/* preferred */}
+            {/* <ExpenseItem></ExpenseItem> */}
+        </div>
+    );
 }
 
 export default App;
--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-  return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Expense Title</h2>
-        <div>Amount</div>
-      </div>
-    </div>
-  );
+    const birthday = new Date(2021, 1, 27); // = 2021-02-26 :D? 
+    const expenseTitle = 'Curnon watch';
+    const price = 3000000;
+    const formattedPrice = price.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
+
+    return (
+        <div className="expense-item">
+            <div>{birthday.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{formattedPrice}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
