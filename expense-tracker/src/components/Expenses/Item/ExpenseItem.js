7aad75ead710a93b3733a81b0e1403e904c5c7fb
hoangdesu <hoangdesu@gmail.com>
2021-10-22 02:12:49 +0700

    48. useState Hook

--- expense-tracker/src/components/Expenses/Item/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/Item/ExpenseItem.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from '../Date/ExpenseDate';
 import Card from '../../Card/Card';
 
 function ExpenseItem({ props }) {
+    
 
+    // useState Hook
+    const [title, setTitle] = useState(props.title);
+    const [newPrice, setNewPrice] = useState(props.price);
+
+    // event handlers
+    const increaseClickHandler = () => {
+        setNewPrice(props.price += 10000);
+        console.log("Increase", newPrice);
+    }
+    
+    const decreaseClickHandler = () => {
+        setNewPrice(props.price -= 10000);
+        console.log("Decrease", newPrice);
+    }
+    
+    const itemClickHandler = () => {
+        console.log("Clicked on", props.title);
+    }
+    
+    const clickHanlder = () => {
+        setTitle('Updated!!');
+        console.log(title); // notice old value will get log, not updated value
+    }
+    
     // convert to VND
-    const price = props.price.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
-
+    const formattedPrice = newPrice.toLocaleString('vi-VI', {style: 'currency', currency: 'VND'});
+    
     return (
         <Card className="expense-item">
             <ExpenseDate props={props} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">{price}</div>
+            <div onClick={itemClickHandler} className="expense-item__description">
+                <h2>{title}</h2>
+                <button onClick={clickHanlder}>Change title</button>
+                <button className="btn" onClick={decreaseClickHandler}>-</button>
+                <div className="expense-item__price">{formattedPrice}</div>
+                <button className="btn" onClick={increaseClickHandler}>+</button>
             </div>
         </Card>
     );
