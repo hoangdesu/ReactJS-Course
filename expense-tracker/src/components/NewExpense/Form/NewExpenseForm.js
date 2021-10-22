c11d6362fcc980e5f2191cc22cab9573ccd3ee8c
hoangdesu <hoangdesu@gmail.com>
2021-10-23 02:59:29 +0700

    53. Mulitple states approach

--- expense-tracker/src/components/NewExpense/Form/NewExpenseForm.js
+++ expense-tracker/src/components/NewExpense/Form/NewExpenseForm.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewExpenseForm.css';
 
 const NewExpenseForm = () => {
+    // useState Hooks
+    const [inputItem, setInputItem] = useState('');
+    const [inputPrice, setInputPrice] = useState('');
+    const [inputDate, setInputDate] = useState('');
 
+    // state change handlers
     const changeItemHandler = (evt) => {
-        console.log(evt.target.value);
-    }
+        setInputItem(evt.target.value);
+    };
+
+    const changePriceHandler = (evt) => {
+        setInputPrice(evt.target.value);
+    };
+
+    const changeDateHandler = (evt) => {
+        setInputDate(evt.target.value);
+    };
 
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Item</label>
-                    <input type="text" onChange={changeItemHandler}/>
+                    <input type="text" onChange={changeItemHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Price</label>
-                    <input type="number" min="1" step="1" />
+                    <input
+                        type="number"
+                        min="1"
+                        step="1"
+                        onChange={changePriceHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2020-01-01" max="2022-12-31" />
+                    <input
+                        type="date"
+                        min="2020-01-01"
+                        max="2022-12-31"
+                        onChange={changeDateHandler}
+                    />
                 </div>
             </div>
             <div className="new-expense__actions">
