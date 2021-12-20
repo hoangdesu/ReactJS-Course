99931a3c88eb6ac20a98b44360b89db3a3340d4c
hoangdesu <hoangdesu@gmail.com>
2021-12-21 01:07:35 +0700

    CSS modules completed

--- goals-digger/src/components/CourseGoals/CourseInput/CourseInput.js
+++ goals-digger/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,46 +1,57 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import AnotherButton from '../../UI/Button/AnotherButton';
 
 import Button from '../../UI/Button/Button';
 import ClearButton from '../../UI/Button/ClearButton';
 import './CourseInput.css';
 
 const FormControl = styled.div`
-
     margin: 0.5rem 0;
-  }
-  
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-  
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid #ccc;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-  
-  &.invalid label {
-    color: red;
-  }
-  
-  &.invalid input {
-    background-color: salmon;
-    border: red;
-  }
+
+    & label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 0.5rem;
+        color: ${(props) => !props.valid && 'red'};
+    }
+
+    & input {
+        display: block;
+        width: 100%;
+        border: 1px solid ${(props) => (props.valid ? '#ccc' : 'red')};
+        font: inherit;
+        line-height: 1.5rem;
+        padding: 0 0.25rem;
+        background: ${(props) => !props.valid && 'salmon'};
+    }
+
+    & input:focus {
+        outline: none;
+        background: #fad0ec;
+        border-color: #8b005d;
+    }
+
+    //   &.invalid label {
+    //     color: red;
+    //   }
+
+    //   &.invalid input {
+    //     background-color: salmon;
+    //     border: red;
+    //   }
 `;
 
+const ButtonGroup = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+    }
+`;
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -71,21 +82,24 @@ const CourseInput = (props) => {
         <form onSubmit={formSubmitHandler}>
             {/* <div className={`form-control ${isValid ? '' : 'invalid'}`}> */}
             {/* <FormControl className={!isValid && 'invalid'}> {/* 1st way: adding class */}
-            <FormControl>
-                <label>
-                    Course Goal
-                </label>
+            <FormControl valid={isValid}>
+                <label>Course Goal</label>
+                <AnotherButton type="button" onClick={() => alert('sup!')}>
+                    hi!
+                </AnotherButton>
                 <input
                     type="text"
                     value={enteredValue}
                     onChange={goalInputChangeHandler}
                 />
-                </FormControl>
+            </FormControl>
             {/* </div> */}
-            <div style={{ width:'100%', display: 'flex', flexDirection:'row', justifyContent: 'space-between' }}>
+            <ButtonGroup>
                 <Button type="submit">Add Goal</Button>
-                <ClearButton type="button" onClick={onClearHandler} >Clear</ClearButton>
-            </div>
+                <ClearButton type="button" onClick={onClearHandler}>
+                    Clear
+                </ClearButton>
+            </ButtonGroup>
         </form>
     );
 };
---
+++ goals-digger/src/components/UI/Button/AnotherButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './AnotherButton.module.css';
+
+const AnotherButton = (props) => {
+    return (
+        <button type={props.type} onClick={props.onClick} className={styles.button}>
+            {props.children}
+        </button>
+    )
+}
+
+export default AnotherButton
