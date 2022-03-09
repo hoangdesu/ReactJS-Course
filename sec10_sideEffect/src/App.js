c197b81c732aa94783f5b23b852a05996b224ab3
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-03-09 11:28:08 +0700

    122. Using Context Consumer

--- sec10_sideEffect/src/App.js
+++ sec10_sideEffect/src/App.js
@@ -1,41 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
   // only runs once when the component did mount
   useEffect(() => {
-    if (localStorage.getItem('loggedIn') === '1') {
+    if (localStorage.getItem("loggedIn") === "1") {
       setIsLoggedIn(true);
     }
-
   }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
-    
+
     // store login state in local storage
-    localStorage.setItem('loggedIn', '1');
+    localStorage.setItem("loggedIn", "1");
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
-    localStorage.removeItem('loggedIn');
+    localStorage.removeItem("loggedIn");
   };
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+        }}
+      >
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
