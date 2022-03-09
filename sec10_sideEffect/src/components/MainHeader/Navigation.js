53b673ac1e0afd6bfaedaebec009f554dd13b48c
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-03-09 11:53:16 +0700

    124. using useContext() hook

--- sec10_sideEffect/src/components/MainHeader/Navigation.js
+++ sec10_sideEffect/src/components/MainHeader/Navigation.js
@@ -1,35 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = () => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
-        )
-      }}
-    </AuthContext.Consumer>
+    // -- METHOD 1: USING CONSUMER
+    // <AuthContext.Consumer>
+    //   {(ctx) => {
+    //     return (
+    //       <nav className={classes.nav}>
+    //       <ul>
+    //         {ctx.isLoggedIn && (
+    //           <li>
+    //             <a href="/">Users</a>
+    //           </li>
+    //         )}
+    //         {ctx.isLoggedIn && (
+    //           <li>
+    //             <a href="/">Admin</a>
+    //           </li>
+    //         )}
+    //         {ctx.isLoggedIn && (
+    //           <li>
+    //             <button onClick={props.onLogout}>Logout</button>
+    //           </li>
+    //         )}
+    //       </ul>
+    //     </nav>
+    //     )
+    //   }}
+    // </AuthContext.Consumer>
+
+    // -- METHOD 2: USING USECONTEXT
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
