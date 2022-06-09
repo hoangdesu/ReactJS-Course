c7dc806c750a448985e9058a5e14ff7b0ad6e119
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-06-09 23:58:19 +0700

    149. added bump animation with useEffect. Aaaaaand we're doneeee

--- 11_FoodOrderApp/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ 11_FoodOrderApp/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
@@ -6,7 +6,9 @@ import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = () => {
     const cartCtx = useContext(CartContext);
-    
+    const [btnClasses, setBtnClasses] = useState(classes.button); // my approach
+    const [btnAnimated, setBtnAnimated] = useState(false); // video approach
+
     const cartItemsNum = cartCtx.items.reduce((currentValue, item) => {
         // console.log('currentValue: ', currentValue);
         // console.log('item amount:', item.amount);
@@ -15,18 +17,51 @@ const HeaderCartButton = () => {
 
     console.log('cartItemsNum:', cartItemsNum);
 
-    // currentValue will get carried on every execution. 
+    // currentValue will get carried on every execution.
     // Initially currentValue = 0
 
+    // video approach
+    const cartBtnClasses = `${classes.button} ${
+        btnAnimated ? classes.bump : ''
+    }`;
+
+    useEffect(() => {
+        // my approach
+        if (cartCtx.items.length === 0) return;
+
+        // play the animation
+        setBtnClasses(`${classes.button} ${classes.bump}`);
+
+        // reset the animation
+        const timer = setTimeout(() => {
+            setBtnClasses(classes.button);
+        }, 300);
+
+        // clean up function
+        // return () => {
+        //     // clean the effect re-run when we clear the time out
+        //     // somehow it stops the rapid animation :(
+        //     clearTimeout(timer);
+        // }
+
+
+        // videos approach
+        // if (cartCtx.items.length === 0) return;
+        // setBtnAnimated(true);
+        // setTimeout(() => {
+        //     setBtnClass(classes.button);
+        //     setBtnAnimated(false);
+        // });
+    }, [cartCtx.items]);
+
     return (
-        <button className={classes.button} onClick={cartCtx.toggleCartOverlay}>
+        <button className={btnClasses} onClick={cartCtx.toggleCartOverlay}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>{cartItemsNum}</span>
         </button>
-        
     );
 };
 
